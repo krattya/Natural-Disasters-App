2928482005db5f8fba4584e7a0bc8d949beb4d7b
Sam Heymann <sam.heymann@stud.h-da.de>
2024-04-05 14:12:52 +0200

    refactor endpoint

--- fastapi/app/endpoints/alerts/usgov/main.py
+++ fastapi/app/endpoints/alerts/usgov/main.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+import app.endpoints.alerts.usgov.crud as usgov_crud
 
 router = APIRouter()
 
 
-@router.get("/")
-def get_usgov_alerts():
-    return {"message": "US Government Alerts"}
+@router.get("")
+def get_usgov_alerts(request: Request):
+    return usgov_crud.get_all_usgov_alerts(request)
 
 
 @router.get("/{alert_id_usgov}/")
-def get_usgov_alert(alert_id_usgov: str):
-    return {"message": f"US Government Alert {alert_id_usgov}"}
+def get_usgov_alert(request: Request, alert_id_usgov: str):
+    return usgov_crud.get_usgov_alert(alert_id_usgov, request)
