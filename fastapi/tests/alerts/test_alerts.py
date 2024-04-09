f4520b4cace1093da5bce9fc7387dd4c4a33babe
Sam Heymann <sam.heymann@stud.h-da.de>
2024-04-09 18:36:14 +0200

    feat(api): add tests

---
+++ fastapi/tests/alerts/test_alerts.py
@@ -0,0 +1,11 @@
+from app.main import app
+from fastapi.testclient import TestClient
+# PYTHONPATH=. pytest
+#isntall pytest
+#pip install httpx
+#pip install -U pytest
+def test_alerts():
+    with TestClient(app) as client:
+        response = client.get("/api/v1/alerts/")
+        assert response.status_code == 200
+        #assert response.json() == {"message": "Alerts!"}
