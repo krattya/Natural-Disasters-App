36297516c07117996a3cd4724089ca87b32200db
Sam Heymann <sam.heymann@stud.h-da.de>
2024-04-17 08:26:39 +0200

    feat(test): add tests for alerts endpoint

--- fastapi/tests/alerts/test_alerts.py
+++ fastapi/tests/alerts/test_alerts.py
@@ -6,8 +6,10 @@ from fastapi.testclient import TestClient
 # isntall pytest
 # pip install httpx
 # pip install -U pytest
+
+
 def test_alerts():
     with TestClient(app) as client:
         response = client.get("/api/v1/alerts/")
         assert response.status_code == 200
-        # assert response.json() == {"message": "Alerts!"}
+        assert isinstance(response.json(), list)
---
+++ fastapi/tests/alerts/test_gdacs.py
@@ -0,0 +1,20 @@
+from app.main import app
+from fastapi.testclient import TestClient
+import pytest
+
+pytest.gdacs_id = ""
+
+
+def test_get_all():
+    with TestClient(app) as client:
+        response = client.get("/api/v1/alerts/gdacs")
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+        pytest.gdacs_id = response.json()[0]["id"]
+
+
+def test_get_one():
+    with TestClient(app) as client:
+        response = client.get(f"/api/v1/alerts/gdacs/{pytest.gdacs_id}/")
+        assert response.status_code == 200
+        assert isinstance(response.json(), dict)
