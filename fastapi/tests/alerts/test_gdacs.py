f970ccb96d0915874bdb37f840a50e1070542ae5
Sam Heymann <sam.heymann@stud.h-da.de>
2024-04-17 08:39:16 +0200

    feat(test): update tests for ci

--- fastapi/tests/alerts/test_gdacs.py
+++ fastapi/tests/alerts/test_gdacs.py
@@ -10,10 +10,13 @@ def test_get_all():
         response = client.get("/api/v1/alerts/gdacs")
         assert response.status_code == 200
         assert isinstance(response.json(), list)
-        pytest.gdacs_id = response.json()[0]["id"]
+        if len(response.json()) > 0:
+            pytest.gdacs_id = response.json()[0]["id"]
 
 
 def test_get_one():
+    if pytest.gdacs_id == "":
+        pytest.skip("No gdacs alerts found")
     with TestClient(app) as client:
         response = client.get(f"/api/v1/alerts/gdacs/{pytest.gdacs_id}/")
         assert response.status_code == 200
--- fastapi/tests/alerts/test_usgov.py
+++ fastapi/tests/alerts/test_usgov.py
@@ -10,10 +10,13 @@ def test_get_all():
         response = client.get("/api/v1/alerts/gdacs")
         assert response.status_code == 200
         assert isinstance(response.json(), list)
-        pytest.usgov_id = response.json()[0]["id"]
+        if len(response.json()) > 0:
+            pytest.usgov_id = response.json()[0]["id"]
 
 
 def test_get_one():
+    if pytest.gdacs_id == "":
+        pytest.skip("No usgov alerts found")
     with TestClient(app) as client:
         response = client.get(f"/api/v1/alerts/gdacs/{pytest.usgov_id}/")
         assert response.status_code == 200
