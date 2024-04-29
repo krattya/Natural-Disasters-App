b5cd2d24fb9d3c35db8e1f819643fc0288d2796f
Sam Heymann <sam.heymann@stud.h-da.de>
2024-04-29 13:59:46 +0200

    feat time

--- collection-services/app/Event.py
+++ collection-services/app/Event.py
@@ -9,7 +9,7 @@ import time
 class Event_Mod(BaseModel):
     gdacs_id: str
     usgov_id: str
-    last_updated: str = None
+    last_updated: float = None
 
 
 class Event(BaseAPI):
@@ -21,8 +21,24 @@ class Event(BaseAPI):
         db_connect = MongoClient("mongodb://root:example@mongo:27017/")
         self._db_db = db_connect["disaster_information"]
 
-    def event_match(self, data1, data2):
-        return True
+    def pase_gdacs_time(self, gdacs_time):
+        return time.time()
+
+    def event_match(self, gdac, usgov) -> bool:
+        # unix time to python time
+        usgov_time = time.gmtime(usgov["properties"]["time"] / 1000)
+        print(f"gdac_time: {usgov_time}")
+        gdacs_info_with_event_time = self.pase_gdacs_time(gdac["summary"])
+        gdacs_time = self.pase_gdacs_time(gdacs_info_with_event_time)
+
+        if abs(gdacs_time - usgov["properties"]["time"]) <= 500:
+            return True
+        else:
+            return False
+
+    def is_event_exists(self, key, id) -> bool:
+        event = self._db_db["events"].find_one({key: id})
+        return event is not None
 
     def getData(self) -> List:
         print("[x] Creating events from matched data")
@@ -39,8 +55,11 @@ class Event(BaseAPI):
                     )
                     json_event = event.model_dump()
                     logging.info(f"Event matched: {json_event}")
-                    result = self._db_db["events"].insert_one(json_event)
-                    print(f"Event matched insert id: {result.inserted_id}")
+                    if not self.is_event_exists(
+                        "gdacs_id", event.gdacs_id
+                    ) and self.is_event_exists("usgov_id", event.usgov_id):
+                        result = self._db_db["events"].insert_one(json_event)
+                        print(f"Event matched insert id: {result.inserted_id}")
 
     def _collectData(self):
         pass
